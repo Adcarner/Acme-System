a029b8f67dc857c7aa7009d2fdc8d8d3d1b31e5c
Andrew Carner <carner3andrew@gmail.com>
2023-08-28 00:14:54 -0400

    Added whatfor method

--- ACME-System/src/ver1/HelloWorld.java
+++ ACME-System/src/ver1/HelloWorld.java
@@ -1,6 +1,11 @@
 package ver1;
 
 public class HelloWorld {
+	public static void whatFor() {
+		String Msg = "What For!";
+		System.out.println(Msg);
+	}
+	
 	public static void heyNow() {
 		String Msg = "Hey Now!!";
 		System.out.println(Msg);
